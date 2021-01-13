cde43fdca23956df0dabc4575a5656f9c084bf64
SE2Dev <brokensilencedev@gmail.com>
2021-01-13 03:23:23 +0000

    Automatically Resolve Version Identifier on Build

--- setup.py
+++ setup.py
@@ -1,8 +1,35 @@
+import os.path
+import re
+
 from setuptools import setup, find_packages
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version_raw(rel_path):
+    rxVersion = "\\(\\s*(\\d+)\\s*,\\s*(\\d+)\\s*,\\s*(\\d+)\\s*\\)"
+    rxVersionLine = "^\\s*__version__\\s*=\\s*" + rxVersion + "(?:\\s*#.*)?"
+    for line in read(rel_path).splitlines():
+        if match := re.search(rxVersionLine, line, re.IGNORECASE):
+            return (int(match.group(1)),
+                    int(match.group(2)),
+                    int(match.group(3)))
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+def get_version(rel_path):
+    version = get_version_raw(rel_path)
+    return ".".join([str(part) for part in version])
+
 
 setup(
     name="PyCoD",
-    version="0.1.0",
+    version=get_version("./src/PyCoD/__init__.py"),
     author="SE2Dev",
     author_email="no-reply@github.com",
     url="https://github.com/SE2Dev/PyCoD",
--- src/PyCoD/__init__.py
+++ src/PyCoD/__init__.py
@@ -4,7 +4,10 @@ from .xmodel import Model
 from .xanim import Anim
 from .sanim import SiegeAnim
 
-version = (0, 3, 0)  # Version specifier for PyCoD
+__version__ = (0, 3, 0)  # Version specifier for PyCoD
+
+# Included for support with legacy versions
+version = __version__
 
 assert Model
 assert Anim
